f2771dd1cf738ba06997f4b58f8c4af61810dd8e
DevLeandroSantos <jose.martins@accountfy.com>
2022-07-31 00:33:49 -0300

    removing js files

--- src/MockTicket/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ticket = void 0;
-const Paymentmethods_1 = require("../Paymentmethods");
-// @ts-ignore: Unreachable code error
-exports.ticket = {
-    method: new Paymentmethods_1.DebitPayment,
-    paymentValue: '2.300,00'
-};
